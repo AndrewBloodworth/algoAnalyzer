a3e8fec463ab0b7ccb1aed5386c3b99247671bbb
AndrewBloodworth <andrewcbld@gmail.com>
2021-09-25 01:48:05 -0600

    Can add lines via Enter key

--- client/interface/Main.js
+++ client/interface/Main.js
@@ -1,35 +1,54 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 const Main = () => {
+  const [lines, setLines] = useState([
+    { raw: "abcdefg" },
+    { raw: "hijklmn" },
+    { raw: "opqrstu" },
+    { raw: "vwxyz" },
+  ]);
+  const handleKeyPress = (event) => {
+    event.preventDefault();
+    if (event.key === "Enter") {
+      console.log("nl");
+
+      setLines([...lines, "new line"]);
+    }
+  };
   return (
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "flex-start",
         height: "100vh",
         width: "80vw",
-        backgroundColor: "blue",
       }}
     >
       <h1>hello</h1>
-      <section
-        style={{
-          width: "100%",
-          height: 30,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "flex-end",
-        }}
-      >
-        <input
-          type="text"
+      {lines.map((line, idx) => (
+        <section
+          key={idx}
           style={{
             width: "100%",
-            height: "100%",
+            height: 30,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flex-end",
           }}
-        />
-      </section>
+        >
+          <input
+            type="text"
+            defaultValue={line.raw}
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            onKeyPress={handleKeyPress}
+          />
+        </section>
+      ))}
     </div>
   );
 };
